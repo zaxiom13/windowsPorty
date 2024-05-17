bda0c1fe2d9f08c55f7f3a5075269a24adb8e4c6
Zak Zahid <z.axiom.413@gmail.com>
2024-05-17 18:00:03 +0100

    multiple tiles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,26 +2,50 @@
 	import { onMount } from 'svelte';
 	import Taskbar from './Taskbar.svelte';
 	import Tile from './Tile.svelte';
-  
+
 	let desktop: HTMLDivElement;
-  
+	let tiles: Array<{ id: number, x: number, y: number }> = [];
+	let nextTileId = 0;
+
 	onMount(() => {
-	  // Additional setup if necessary
+		// Add initial tiles if necessary
+		addTile();
 	});
-  </script>
-  
-  <div class="desktop" bind:this={desktop}>
-	<Tile {desktop} />
-	<Taskbar />
-  </div>
-  
-  <style>
-	.desktop {
-	  width: 100%;
-	  height: 100vh;
-	  border: 1px solid #000;
-	  position: relative;
-	  overflow: hidden;
+
+	function addTile() {
+		tiles = [...tiles, { id: nextTileId++, x: 50, y: 50 }];
 	}
-  </style>
-  
+</script>
+
+<div class="desktop" bind:this={desktop}>
+	{#each tiles as tile}
+		<Tile {desktop} id={tile.id} x={tile.x} y={tile.y} />
+	{/each}
+	<Taskbar />
+	<button class="add-tile-button" on:click={addTile}>Add Tile</button>
+</div>
+
+<style>
+	.desktop {
+		width: 100%;
+		height: 100vh;
+		border: 1px solid #000;
+		position: relative;
+		overflow: hidden;
+	}
+	.add-tile-button {
+		position: absolute;
+		top: 10px;
+		right: 10px;
+		z-index: 10;
+		background-color: #3498db;
+		color: white;
+		border: none;
+		padding: 10px;
+		cursor: pointer;
+		border-radius: 5px;
+	}
+	.add-tile-button:hover {
+		background-color: #2980b9;
+	}
+</style>
--- src/routes/Tile.svelte
+++ src/routes/Tile.svelte
@@ -2,64 +2,63 @@
 	import { onMount } from 'svelte';
 	import ResizeHandle from './ResizeHandle.svelte';
 	import { Tile as TileClass } from './../Tile';
-  
+
 	export let desktop: HTMLDivElement;
+	export let id: number;
+	export let x: number = 0;
+	export let y: number = 0;
+
 	let tile: HTMLDivElement;
 	let tileInstance: TileClass;
-  
+
 	const directions = [
-	  'top-left',
-	  'top-right',
-	  'bottom-left',
-	  'bottom-right',
-	  'top',
-	  'bottom',
-	  'left',
-	  'right'
+		'top-left', 'top-right', 'bottom-left', 'bottom-right',
+		'top', 'bottom', 'left', 'right'
 	];
-  
+
 	onMount(() => {
-	  tileInstance = new TileClass(tile, desktop);
+		tileInstance = new TileClass(tile, desktop);
+		tile.style.left = `${x}px`;
+		tile.style.top = `${y}px`;
 	});
-  
+
 	function handleMouseLeave() {
-	  tileInstance?.handleMouseLeave();
+		tileInstance?.handleMouseLeave();
 	}
-  </script>
-  
-  <div class="tile" bind:this={tile} on:mouseleave={handleMouseLeave}>
+</script>
+
+<div class="tile" bind:this={tile} on:mouseleave={handleMouseLeave}>
 	<div class="titlebar">Title Bar</div>
 	<div class="body">Tile Body</div>
 	{#each directions as direction}
-	  <ResizeHandle {direction} />
+		<ResizeHandle {direction} />
 	{/each}
-  </div>
-  
-  <style>
+</div>
+
+<style>
 	.tile {
-	  width: 200px;
-	  height: 200px;
-	  background-color: #3498db;
-	  position: absolute;
-	  top: 0;
-	  left: 0;
-	  display: flex;
-	  flex-direction: column;
-	  min-width: 100px;
-	  min-height: 100px;
-	  max-width: 500px;
-	  max-height: 500px;
+		width: 200px;
+		height: 200px;
+		background-color: #3498db;
+		position: absolute;
+		top: 0;
+		left: 0;
+		display: flex;
+		flex-direction: column;
+		min-width: 100px;
+		min-height: 100px;
+		max-width: 500px;
+		max-height: 500px;
 	}
-  
+
 	.titlebar:active {
-	  cursor: grabbing;
+		cursor: grabbing;
 	}
-  
+
 	.body {
-	  width: 100%;
-	  height: calc(100% - 30px);
-	  background-color: #ecf0f1;
-	  flex: 1;
+		width: 100%;
+		height: calc(100% - 30px);
+		background-color: #ecf0f1;
+		flex: 1;
 	}
-  </style>
-  
+</style>
